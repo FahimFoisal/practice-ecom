b90d6edf64486c7145c5cecacfb25b99f619441e
unknown <fahimuschas@gmail.com>
2023-08-15 20:47:17 +0600

    shop page developing

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Checkbox } from 'antd';
+import Jumbotron from '../components/cards/Jumbotron';
+import { useAuth } from '../context/auth';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
     const [categories,setCategories] = useState([]);
     const [checked,setChecked] = useState([]);
     const [radio,setRadio] = useState([]);
+    const [auth,setAuth] = useAuth('');
 
     useEffect(()=>{
         loadCategoris();
@@ -30,10 +34,32 @@ const Shop = () => {
         }
     }
 
+    const handleCheck = (e,id) => {
+        console.log(`checked = ${e.target.checked}`,id);
+      };
+
     return (
-        <div>
-            <h1>shop page</h1>
-        </div>
+        <>
+            <Jumbotron title={auth?.user?.name}/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
+                            Filter by Categories
+                        </h2>
+                        <div className="row">
+                            {
+                                categories.map((c) => (<Checkbox key={c._id} onChange={(e)=>handleCheck(e,c._id)}>{c.name}</Checkbox>))
+                            }
+                        </div>
+                    </div>
+                    <div className="col-md-9">
+                        <div className="bg-light p-3 h4">Admin Info</div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
